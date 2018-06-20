13e024244b4c4c204a3768daffd150e729bd665e
linbc <linbc@aliyun.com>
2018-06-20 23:29:06 +0800

    先给挪出来

---
+++ lua-ahocorasick-0.1.rockspec
@@ -0,0 +1,53 @@
+package = "lua-ahocorasick"
+version = "0.1"
+source = {
+   url = "git+https://github.com/linbc/nginx-lua-ahocorasick.git",
+   tag = "0.1",
+}
+
+description = {
+    summary = "A fast Aho–Corasick lua module",
+    detailed = [[
+        The Lua Aho–Corasick module provides Aho–Corasick support for Lua. It features:
+        - Fast, standards compliant Aho–Corasick_algorithm
+        - No dependencies on other libraries
+    ]],
+    homepage = "https://github.com/linbc/nginx-lua-ahocorasick.git",
+    license = "MIT"
+}
+
+dependencies = {
+    "lua >= 5.1"
+}
+
+build = {
+    type = "builtin",
+    modules = {
+        ahocorasick = {
+            sources = { "ac_mem_pool.c", "aho_corasick.c", "node.c" },
+            defines = {
+-- LuaRocks does not support platform specific configuration for Solaris.
+-- Uncomment the line below on Solaris platforms if required.
+--                "USE_INTERNAL_ISINF"
+            }
+        }
+    },
+    install = {
+        lua = {
+            ["cjson.util"] = "lua/cjson/util.lua"
+        },
+        bin = {
+            json2lua = "lua/json2lua.lua",
+            lua2json = "lua/lua2json.lua"
+        }
+    },
+    -- Override default build options (per platform)
+    platforms = {
+        win32 = { modules = { ahocorasick = { defines = {
+            "DISABLE_INVALID_NUMBERS", "USE_INTERNAL_ISINF"
+        } } } }
+    },
+    copy_directories = { "tests" }
+}
+
+-- vi:ai et sw=4 ts=4:
--- src/ac_helper.cpp
+++ src/ac_helper.cpp
@@ -28,28 +28,31 @@ int match_handler(MATCH *m, void *param)
 	return 0;
 }
 
+//全局输出缓冲区
+static int s_output_len = 0;
+static char *s_output = NULL;
 
-struct __AC_AUTOMATA
-{
-	__AC_AUTOMATA()
-	{
+struct __AC_AUTOMATA {
+	__AC_AUTOMATA() {
 		g_aca = new AC_AUTOMATA;
 		ac_automata_init(g_aca, &match_handler);
 	}
 
-	~__AC_AUTOMATA()
-	{
+	~__AC_AUTOMATA() {
 		if (g_fuck_pingbi!=NULL) 
 			free(g_fuck_pingbi);
-		if (g_aca != NULL)
-		{
+		if (g_aca != NULL) {
 			ac_automata_release(g_aca);
 			delete g_aca;
+		}
+		if (s_output){
+			free(s_output);
+			s_output_len = 0;
 		}
 	}
 }__AC_AUTOMATA;
 
-extern "C" int LoadFuckPingbi(const char* cpath)
+extern "C" int LoadPingbi(const char* cpath)
 {
 	if(g_fuck_pingbi != NULL)
 	{
@@ -111,7 +114,7 @@ extern "C" int LoadFuckPingbi(const char* cpath)
 }
 
 extern "C" int FuckPingbi(char* pinbi_buff)
-{	
+{
 	STRING tmp_str;
 	tmp_str.str = pinbi_buff;
 	tmp_str.length = strlen(tmp_str.str);
@@ -124,6 +127,69 @@ extern "C" int FuckPingbi(char* pinbi_buff)
 	return param.ret;
 }
 
+//////////////////////////////////////////////////////////////////////////////
+
+
+//如果缓冲区太小则扩大
+static int checkout_output(int newsize) {
+	//如果内存还没有分配则重新分配一下
+	if (s_output_len >= newsize)
+		return 0;
+	
+	if (s_output_len == 0) {
+		s_output_len = newsize;
+		s_output = malloc(s_output_len);
+	}
+	while (s_output_len < newsize) {
+		s_output_len = s_output_len << 1;
+	}
+	s_output = realloc(s_output, s_output_len);
+	return 1;
+}
+
+static int lua_load(lua_State *L) {
+	// 传入路径名
+	const char* path = luaL_checkstring(L, 1);
+	int ret = LoadPingbi(path);	
+	if(ret != 0) {
+		lua_pushboolean(L, FALSE);
+		lua_pushnumber(L, ret);
+		return 2;
+	} else {
+		lua_pushboolean(L, TRUE);
+		return 1;
+	}	
+}
+
+static int lua_match(lua_State *L) {
+	// 传入文本
+	const char* str = luaL_checkstring(L, 1);
+	int len = strlen(str) + 1;
+	checkout_output(len);	
+	memcpy(s_output, str, len);
+	
+	// 返回匹配结果
+	// 如果匹配上返回新值
+	if (FuckPingbi(s_output) != FALSE)
+		lua_pushboolean(L, TRUE);
+	else
+		lua_pushboolean(L, FALSE);
+	lua_pushstring(L, s_output);
+	return 2;
+}
+
+static const struct luaL_reg _funcs[] = {
+	{ "load",				&lua_load },
+	{ "match",				&lua_match },
+	{ NULL, NULL }
+};
+
+extern "C" int luaopen_ahocorasick_binding(lua_State *L) {
+	int startPos = lua_gettop(L);
+	luaL_openlib(L, "ahocorasick", _funcs, 0);
+	lua_settop(L, startPos);
+	return 0;
+}
 
 void Test()
 {
