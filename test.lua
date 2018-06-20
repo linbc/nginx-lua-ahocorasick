3a4ae8780d143882edfabaed23f8e71ac31b0eb6
linbc <linbc@aliyun.com>
2018-06-20 14:06:40 +0800

    挪挪位置

--- test.lua
+++
@@ -1,6 +0,0 @@
-local libahocorasick = require 'ahocorasick'
-
-local ret = libahocorasick.Load('_fuck_pingbi.txt')
-print(ret)
-print(libahocorasick.Pingbi('a493664527h'))
-
---
+++ test/test.lua
@@ -0,0 +1,6 @@
+local libahocorasick = require 'ahocorasick'
+
+local ret = libahocorasick.Load('_fuck_pingbi.txt')
+print(ret)
+print(libahocorasick.Pingbi('a493664527h'))
+
